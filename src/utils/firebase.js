9fb5e812ae913c72c5a936ccdfc83db53fb50984
btkkzc <btkkzc@gmail.com>
2022-04-17 13:23:04 +0200

    api key added to env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBYBn3lxGkj8aqeYjvveOGNnETmXt3f7U",
-  authDomain: "firecontactbase-project.firebaseapp.com",
-  databaseURL: "https://firecontactbase-project-default-rtdb.firebaseio.com",
-  projectId: "firecontactbase-project",
-  storageBucket: "firecontactbase-project.appspot.com",
-  messagingSenderId: "748306189504",
-  appId: "1:748306189504:web:3a93154d5e2c30425eb125"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
